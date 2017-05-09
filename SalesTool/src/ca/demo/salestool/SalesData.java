05d2c7f2ec749db39024eec5d09992774e41ef71
Teodoro Orlow Wey <teowey@gmail.com>
2017-05-09 15:52:55 +0200

    Completed displaying data in SalesData.java

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -6,8 +6,9 @@ public class SalesData {
 	int data[] = {0, 4, 42};
 	
 	public void display() {
+		System.out.println("Data: ");
 		for (int i = 0; i < data.length; i++) {
-			System.out.println("");
+			System.out.println("Next value: " + data[i]);
 		}
 	}
 }
